f772ebe67b63b8cfe263020bd222daf82e11c84d
Ronan Pozzi <contact@treenity-web.fr>
2016-09-03 15:59:54 +0200

    Clean and optimize

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,43 +2,27 @@
  * Created by treen on 03/09/2016.
  */
 jQuery(document).ready(function ($) {
-    var supportBlock = $('#bbpcs');
 
-    supportBlock.on('click', 'button#scanparser', function (e) {
-        var items, matches, id = 0;
-
-        items = [
-            'wp_version',
-            'php_mysql',
-            'theme_name',
-            'theme_uri',
-            'plugins',
-            'uri',
-            'host'
-        ];
-
+    // Onsubmit, parse datas and populate fields
+    $('#bbpcs').on('click', 'button#scanparser', function (e) {
         e.preventDefault();
-        var find = /- (.*) : (.*)/m;
-        var regex = new RegExp(find);
         var currentVal = $('#support_parser').val();
-        currentVal = currentVal.split('\n');
-        currentVal.forEach(function (item) {
-            if (regex.test(item)) {
-                matches = item.match(find);
-                $('[data-id="' + items[id] + '"]').val(matches[2]);
-                console.log(items[id]);
-                $('.bbpcs__summary__list__item--' + items[id] + '> .bbpcs__summary__list__item__desc').text(matches[2])
-            }
-            id++;
+        var items = parse_item(currentVal);
+        $.each(items, function (key, value) {
+            $('[data-id="' + key + '"]').val(value);
+            $('.bbpcs__summary__list__item--' + key + '> .bbpcs__summary__list__item__desc').text(value)
         });
         $('.bbpcs__panel__content').hide();
         $('.bbpcs__summary').show();
     });
 
+    // When chaings inputs content, update summary
     $('.bbpcs__panel__content__input').on('input', function () {
-        $('.bbpcs__summary__list__item--' + $(this).data('id') + '> .bbpcs__summary__list__item__desc').text($(this).val())
+        var inputId = $('.bbpcs__summary__list__item--' + $(this).data('id'));
+        inputId.find('.bbpcs__summary__list__item__desc').text($(this).val())
     });
 
+    // Panel management
     $('.bbpcs__panel__headers__title').click(function () {
         var panel_prefix = '#bbpcs_panel_';
         var panel = panel_prefix + $(this).data('panel');
@@ -46,6 +30,52 @@ jQuery(document).ready(function ($) {
         $('.bbpcs__panel__headers__title').removeClass('active');
         $(panel).fadeIn();
         $(this).addClass('active');
-    })
+    });
+
+    /*
+     * Function to parse and transform textarea string to object
+     * todo: Change plugin to return normalized keys
+     * */
+    function parse_item(datas) {
+        var matches;
+        var output = {};
+        var pattern = '^- (.*) : (.*)$';
+        var regex = new RegExp(pattern);
+        datas = datas.split('\n');
+        datas.forEach(function (item) {
+            if (regex.test(item)) {
+                matches = item.match(pattern);
+                switch (matches[1]) {
+                    case 'Version de WordPress':
+                        output['wp_version'] = matches[2];
+                        break;
+                    case 'Version de PHP/MySQL':
+                        var php_mysql = matches[2].split(' / ');
+                        output['php_version'] = php_mysql[0];
+                        output['mysql_version'] = php_mysql[1];
+                        break;
+                    case 'Thème utilisé':
+                        output['theme_name'] = matches[2];
+                        break;
+                    case 'Thème URI':
+                        output['theme_uri'] = matches[2];
+                        break;
+                    case 'Extensions en place':
+                        output['plugins'] = matches[2];
+                        break;
+                    case 'Adresse du site':
+                        output['uri'] = matches[2];
+                        break;
+                    case 'Nom de l\'hébergeur':
+                        output['host'] = matches[2];
+                        break;
+                }
+            }
+        });
+        console.log(output);
+
+        return output;
+
+    }
 
 });
