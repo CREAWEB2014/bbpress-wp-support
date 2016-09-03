da5ec158e9bd45148a400f7edf7107113b392782
Ronan Pozzi <contact@treenity-web.fr>
2016-09-03 05:24:55 +0200

    Init plugin

---
+++ wordpress-support.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Wordpress Support
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Add support system for Wordpress-support
+Version: 1.0
+Author: Treenity
+Author URI: http://www.treenit-web.fr
+License: GNU
+*/
